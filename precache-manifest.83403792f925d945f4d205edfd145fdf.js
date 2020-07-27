c769160e213158132228f22c8f175d8d7d7e142f
Zach Alam <zelphics@gmail.com>
2020-07-26 18:54:41 -0700

    Updates

---
+++ precache-manifest.83403792f925d945f4d205edfd145fdf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d26a572c6daf9b104c7e5655101168",
+    "url": "/votes.fyi/index.html"
+  },
+  {
+    "revision": "d09457ee66e310ffdcdd",
+    "url": "/votes.fyi/static/css/main.0ebb0049.chunk.css"
+  },
+  {
+    "revision": "1a3bffbfc988cfc662a0",
+    "url": "/votes.fyi/static/js/2.69173063.chunk.js"
+  },
+  {
+    "revision": "6988bff49f5cdadaf79a7f3e42ad113e",
+    "url": "/votes.fyi/static/js/2.69173063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d09457ee66e310ffdcdd",
+    "url": "/votes.fyi/static/js/main.8efb1edf.chunk.js"
+  },
+  {
+    "revision": "37efed03e9f8bd5c2080",
+    "url": "/votes.fyi/static/js/runtime-main.98c5fcac.js"
+  },
+  {
+    "revision": "63b5b47481d90ba458b9514f22f89fc1",
+    "url": "/votes.fyi/static/media/animat-customize-color.63b5b474.gif"
+  },
+  {
+    "revision": "bc3f4aa72af8cb036df201f704204b77",
+    "url": "/votes.fyi/static/media/default-monochrome.bc3f4aa7.svg"
+  }
+]);
